948fbaa2818e988471aa6fbd8df6e54ddece3437
宋竹梅 <zhumei.song@monstar-lab-cn.com>
2017-09-01 16:04:21 +0800

    9.1 svg

--- js/app.js
+++ js/app.js
@@ -1,51 +1,75 @@
 import React from 'react';
 import {
-    AppRegistry,
-    View,
-    Text,
-    Button
+  AppRegistry,
+  View,
+  Text,
+  Button,
+  Image,
+  StyleSheet
 } from 'react-native';
-import {StackNavigator} from 'react-navigation';
+
+import {TabNavigator} from 'react-navigation';
 
 class HomeScreen extends React.Component {
-    static navigationOptions = {
-        tabBarLabel: 'Home',//showIcon option
-        tabBarIcon: ({tintColor}) => (
-            <Image source={require()}
-        )
-    };
+  static navigationOptions = {
+    tabBarLabel: 'Home',//showIcon option
+    tabBarIcon: ({tintColor}) => (
+      <Image
+        source={require('../img/svg/home.svg')}
+        style={[styles.icon,{tintColor:tintColor}]}
+      />
+    )
+  };
 
-    render() {
-        const {navigate} = this.props.navigation;
-        return (
-            <View>
-                <Text>Hello, Chat App!</Text>
-                <Button
-                    onPress={() => navigate('Chat')}
-                    title="Chat with Lucy"
-                />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <Button
+        onPress={()=>this.props.navigation.navigate('Notifications')}
+        title="go to notifications"
+      />
+    );
+  }
 }
 
-class ChatScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Chat with Lucy',
-    };
+class MyNotificationsScreen extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Notifications',
+    tabBarIcon: ({ tintColor }) => (
+      <Image
+        source={require('../img/svg/my.svg')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
 
-    render() {
-        return (
-            <View>
-                <Text>Chat with Lucy</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <Button
+        onPress={() => this.props.navigation.goBack()}
+        title="Go back home"
+      />
+    );
+  }
 }
 
-const Hope = StackNavigator({
-    Home: {screen: HomeScreen},
-    Chat: {screen: ChatScreen},
+const styles = StyleSheet.create({
+  icon: {
+    width: 26,
+    height: 26,
+  },
+});
+
+const Hope = TabNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+},{
+  tabBarOptions: {
+    activeTintColor: '#e91e63',
+  },
 });
 
 AppRegistry.registerComponent('Hope', () => Hope);
