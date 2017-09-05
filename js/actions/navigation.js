df1e63e85d4c34a0ac4228e1cf725e2f2835547d
宋竹梅 <zhumei.song@monstar-lab-cn.com>
2017-09-05 10:11:06 +0800

    9.5 tabs function & product list

--- js/actions/navigation.js
+++ js/actions/navigation.js
@@ -1,15 +1,10 @@
-// import type { Action } from './types';
-//
-// type Tab = 'schedule' | 'my-schedule' | 'map' | 'notifications' | 'info';
-//
-// module.exports = {
-//   switchTab: (tab: Tab): Action => ({
-//     type: 'SWITCH_TAB',
-//     tab,
-//   }),
-//
-//   switchDay: (day: 1 | 2): Action => ({
-//     type: 'SWITCH_DAY',
-//     day,
-//   }),
-// };
+import type { Action } from './types';
+
+type Tab = '首页' | '购物车' | 'map' | 'notifications' | 'info';
+
+module.exports = {
+  switchTab: (tab: Tab): Action => ({
+    type: 'SWITCH_TAB',
+    tab,
+  }),
+};
---
+++ js/actions/types.js
@@ -0,0 +1,4 @@
+'use strict';
+
+export type Action =
+  {type: 'SWITCH_TAB', tab: 'schedule' | 'my-schedule' | 'map' | 'notifications' | 'info'};
